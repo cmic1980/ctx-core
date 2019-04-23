3345bafb5da6462fe8dca0ad2b59dde6da780772
Yu.miao <yu.miao@hinacom.com>
2019-04-24 00:19:41 +0800

    登录验证

--- src/main/java/sg/ctx/core/security/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package sg.ctx.core.security.repository;
-
-import org.springframework.stereotype.Repository;
-import sg.ctx.core.security.domain.User;
-
-@Repository
-public interface UserRepository {
-    User getUserByUserName(String userName);
-}
